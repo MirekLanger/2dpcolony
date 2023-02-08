7a7df865b68afb7faaaf4106bbdd014cbe0d0703
Daniel Valenta <daniel.valenta@tietoevry.com>
2023-02-08 19:18:30 +0100

    1) Added grid into the canvas for displaying environment 2) Added information about the current / max step (iteration) 3) Added optional function for showing the default position of the agents (red square) 4) Added list of user-defined variables for Visualization
    1) xlsx file can be detected in working directory automatically:
     os.path.dirname(os.path.realpath(__file__)) #Get directory of the current .py script
    2) added new (needed) parameters to function from vizualize.py (steps)
    
    1) set parameters of visualizer via user-defined parameters in xlsx file
    2) add function to show interesting symbols in the environment

--- app/main.py
+++ app/main.py
@@ -2,33 +2,34 @@ import extractExcel
 import colony as col
 import visualize
 import sys
+import os
+
 
 def main(args = sys.argv[1:]):
     if len(args):
         definitionFile = str(args[0])
     else:
-        definitionFile = 'colony.xlsx'
+        pyDir = os.path.dirname(os.path.realpath(__file__)) #Get directory of the current .py script
+        definitionFile = pyDir + '\\colony.xlsx'
     colonieDefinition = extractExcel.getColonie(definitionFile)
     colony = col.Colony(colonieDefinition['environment'], colonieDefinition['agents'])
     step = 0
     steps = colonieDefinition['parameters']['steps']
-    initplot = visualize.initVizualiser()
+    agentCoords = [o.coordinates for o in colony.agent]
+    initplot = visualize.initVizualiser(steps, colony.rowsEnv, colony.colsEnv, agentCoords)
+    
     while step != steps:
         step += 1
         print(step)
         colony.colonyStep()
-        coords = [o.coordinates for o in colony.agent]
-        popData = visualize.initPopulation(colony.rowsEnv, colony.colsEnv, coords)
-        visualize.updatePlot(initplot,popData, colonieDefinition['parameters']['animationDelay'])
+        agentCoords = [o.coordinates for o in colony.agent]
+        popData = visualize.initPopulation(colony.rowsEnv, colony.colsEnv, agentCoords)
+        visualize.updatePlot(step, initplot,popData, colonieDefinition['parameters']['animationDelay'])
+
     visualize.endOfVisualization(initplot, popData)
 
-    print(colony.envMatrix)
-    coords = [o.coordinates for o in colony.agent]
-    print(coords)
-    res = (visualize.initPopulation(colony.rowsEnv, colony.colsEnv, coords))
-    for i in res:
-        print(i)
-    for a in colony.agent:
+    #print(colony.envMatrix) 
+    for a in colony.agent: #Print the last configuration of the agent / can be commented
         print("Agent:")
         print(a.contents)
         print(a.coordinates.get("i"), a.coordinates.get("j"))
--- app/visualize.py
+++ app/visualize.py
@@ -5,72 +5,52 @@ import matplotlib.patches as mpatches
 import numpy
 import sys
 
+"""
+parameters of visualizer:
+1) animation_delay
+2) show_default_position
+3) ... colours? Like: p>green ...
+"""
+
 animation_delay = 0.915 # pause between displaying new iteration in seconds [default: 0.15]
 
-def initPopulation(rows, cols, acoordinates):
+def initPopulation(rows, cols, acoordinates, defaultPosition=False):
     popData = [[0.0 for j in range(0,cols)] for i in range(0,rows)]
     for coordinates in acoordinates:
         coord_i = coordinates.get("j")-1 #switch j and i
         coord_j = coordinates.get("i")-1
         if coord_i in range (0,rows) and coord_j in range (0,cols):
             popData[coord_i][coord_j]=1.0 #Agent is not out of the env
+            if defaultPosition:
+                showDefaultPosition(coord_i,coord_j)
     return popData
 
-def updatePlot(initplot,popData, animation_delay): # function to update 2D plot
+def updatePlot(step,initplot,popData, animation_delay): # function to update 2D plot
     initplot.set_data(popData)
-    rect=mpatches.Rectangle((3-0.5,3-0.5),1,1, fill = False, color = "red",linewidth = 1)
-    pyplot.gca().add_patch(rect)
+    pyplot.title("Iteration "+str(step),
+                fontsize = 24)
+    #rect=mpatches.Rectangle((3-0.5,3-0.5),1,1, fill = False, color = "red",linewidth = 1)
+    #pyplot.gca().add_patch(rect)
     pyplot.pause(animation_delay)
-    [p.remove() for p in reversed(pyplot.gca().patches)]
+    #[p.remove() for p in reversed(pyplot.gca().patches)]
 """    
-    for i in range(1,x_dim):
-        for j in range(1,y_dim):
-            if environmentSymbols[i][j].value == 'P':
+Hint: Colour settings:
                 clrs = {0:"aquamarine", 1:"mediumaquamarine", 2:"lightgreen", 3:"greenyellow", 4:"yellowgreen", 5:"olivedrab", 6:"seagreen",7:"darkgreen", 8:"olive"};
                 clr = "black"
-                sz = int(environmentSymbols[i][j].size)
-                if 0 <= sz <= 5:
-                    clr = clrs[sz]
-                rect=mpatches.Rectangle((int(j)-0.5,int(i)-0.5),1,1, fill = False, color = clr,linewidth = 1)
-                pyplot.gca().add_patch(rect)
-                if environmentSymbols[i][j].size < 1:
-                    environmentSymbols[i][j].value = 'e'
-                else:
-                    environmentSymbols[i][j].size -= rmFI
-            if environmentSymbols[i][j].value == 'p':
                 clrs = {0:"lightpink", 1:"pink", 2:"plum", 3:"violet", 4:"orchid", 5:"darkorchid"};
                 clr = "purple"
-                sz = int(environmentSymbols[i][j].size)
-                if 0 <= sz <= 5:
-                    clr = clrs[sz]
-                rect=mpatches.Rectangle((int(j)-0.5,int(i)-0.5),1,1, fill = False, color = clr,linewidth = 1)
-                pyplot.gca().add_patch(rect)
-            elif (environmentSymbols[i][j].value == 'L' or environmentSymbols[i][j].value == 'R' or environmentSymbols[i][j].value == 'U' or environmentSymbols[i][j].value == 'D'):
                 clrs = {0:"aquamarine", 1:"mediumaquamarine", 2:"lightgreen", 3:"greenyellow", 4:"yellowgreen", 5:"olivedrab", 6:"seagreen",7:"darkgreen", 8:"olive"};
                 clr = "darkgreen"
-                sz = int(environmentSymbols[i][j].size)
-                if 0 <= sz <= 5:
-                    clr = clrs[sz]
                 rect=mpatches.Rectangle((int(j)-0.5,int(i)-0.5),1,1, fill = False, color = clr,linewidth = 1)
                 pyplot.gca().add_patch(rect)
-                if environmentSymbols[i][j].size < 1:
-                    environmentSymbols[i][j].value = 'e'
-                else:
-                    environmentSymbols[i][j].size -= rmFI
-
-    #for prey in preylist:
-    #    rect=mpatches.Rectangle((int(prey[1])-0.5,int(prey[0])-0.5),1,1, fill = False, color = "purple",linewidth = 1)
-    #    pyplot.gca().add_patch(rect)
-    pyplot.pause(animation_delay)
-    [p.remove() for p in reversed(pyplot.gca().patches)]
+                #[p.remove() for p in reversed(pyplot.gca().patches)]
 """
 
-testagent =	{
-  "i": 5,
-  "j": 2,
-}
+def showDefaultPosition(x,y):
+    rect=mpatches.Rectangle((y-0.5,x-0.5),1,1, fill = False, color = "red",linewidth = 1)
+    pyplot.gca().add_patch(rect)
 
-def initVizualiser():
+def initVizualiser(numSteps, rowsEnv, colsEnv, aCoords):
     # using colors from matplotlib, define a color map
     colormap = colors.ListedColormap(["lightgrey","green","blue"])
     # define figure size using pyplot
@@ -78,16 +58,27 @@ def initVizualiser():
     # using pyplot add a title
     pyplot.title("Iteration 0",
                 fontsize = 24)
-    pyplot.suptitle("Simulation is running...", fontsize = 16, ha='center', va='center')
+    subtitle = 'Simulation is running... will run up to ' + str(numSteps) + ' iterations.'
+    pyplot.suptitle(subtitle, fontsize = 16, ha='center', va='center')
     # using pyplot add x and y labels
     pyplot.xlabel("x coordinates", fontsize = 20)
     pyplot.ylabel("y coordinates", fontsize = 20)
     # adjust x and y axis ticks, using pyplot
-    pyplot.xticks(fontsize = 16)
-    pyplot.yticks(fontsize = 16)
+    pyplot.xticks(fontsize = 12)
+    pyplot.yticks(fontsize = 12)
+    ax = fig.gca()
+
+    ax.set_xticks(numpy.arange(0, colsEnv, 1))
+    ax.set_xticks(numpy.arange(-0.5, colsEnv, 1),minor=True)
+    ax.set_xticklabels(numpy.arange(1, colsEnv+1, 1))
+
+    ax.set_yticks(numpy.arange(0, rowsEnv, 1)) # EDIT
+    ax.set_yticks(numpy.arange(-0.5, rowsEnv, 1), minor=True)
+    ax.set_yticklabels(numpy.arange(1, rowsEnv+1, 1))
+    pyplot.grid(which='minor')
     # use .imshow() method from pyplot to visualize agent locations
-    initplot = pyplot.imshow(X = initPopulation(10,7,[testagent]),
-                cmap = colormap)
+    initplot = pyplot.imshow(X = initPopulation(rowsEnv,colsEnv,aCoords,defaultPosition=True), 
+                cmap = colormap) 
     def keypress(keyEvent):
         character = str(keyEvent.key)
         print (character)
@@ -98,23 +89,14 @@ def initVizualiser():
 
     fig.canvas.mpl_connect('key_press_event', keypress)
     pyplot.draw()
-    #pyplot.pause(0.85)
+    pyplot.pause(animation_delay)
     return initplot
 
 def endOfVisualization(initplot, popData):
     initplot.set_data(popData)
-    #END
-    itercount = 1
-    n_iterations = 2
-    #End of simulation
-    newtitle = "Iteration " + str(itercount - 1) + " of " + str(n_iterations) + " [end]"
+    newtitle = str(pyplot.gca().get_title()) + " [END]"
     pyplot.title(newtitle, fontsize = 24)
     pyplot.suptitle("Click anywhere to exit.", fontsize = 16, ha='center', va='center')
     pyplot.waitforbuttonpress(0)
     pyplot.draw()
     print ("[Info] End of simulation")
-    return "a"
-
-initVizualiser()
-#pyplot.draw()
-#endOfVisualization()
