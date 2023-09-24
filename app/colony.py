b5c78d628d5aa2a12e8d0f551ea4eda6bad31211
Mirek <miroslav.langer@vsb.cz>
2023-09-24 14:52:55 +0200

    #10 - update of the computational step. 1) generate a set of applicable programs, 2) set a random order of the agents, 3) try to apply a random program, 4) update environment

--- app/colony.py
+++ app/colony.py
@@ -32,7 +32,7 @@ class Colony:
     def evolveEnvironmet(self):
         for i in range(self.rowsEnv):
             for j in range(self.colsEnv):
-                applicableRules = self.getApplicableRules(i,j)
+                applicableRules = self.getApplicableRules(i, j)
                 if applicableRules:
                     cell = self.envMatrix[i][j]
                     applyRule = applicableRules[random.randrange(len(applicableRules))]
@@ -46,10 +46,16 @@ class Colony:
             self.envMatrix[element['position']['i']][element['position']['j']] += element['symbol']
 
     def agentsAct(self):
+        # generate a set of applicable programs for each agent
+        for i in range(self.numAgents):
+            self.agent[i].getApplicablePrograms()
+        # random order of agents
+        # set a list of indexes of the agents
         agentIndexList = [i for i in range(self.numAgents)]
         while agentIndexList:
+            #choose a random index of the agent
             agentIndex = agentIndexList.pop(random.randrange(len(agentIndexList)))
-            self.agent[agentIndex].applyProgram()
+            self.agent[agentIndex].applyRandomProgram()
 
     def colonyStep(self):
         self.initComputationalStep()
@@ -65,6 +71,7 @@ class Colony:
             self.vicinityLength = 9
             self.colony = col
             self.jokerSymbols = jokerSymbols
+            self.applicablePrograms = []
 
         def getVicinity(self):
             vicinity = []
@@ -76,68 +83,71 @@ class Colony:
         def getEnvironmentContent(self):
             return self.colony.envMatrix[self.coordinates['i']][self.coordinates['j']]
 
-        def getApplicablePrograms(self):
-            aplicablePorgrams = []
-            for program in self.programs:
-                allRulesAplicable = 1
-                innerObjects = self.contents[:]
-                envContent = self.getEnvironmentContent()[:]
-                for rule in program:
-                    #get type of a rule
-                    #motion
-                    if rule['operator'] in ['u', 'd', 'r', 'l']:
-                        vicinity = self.getVicinity()
-                        for i in range(self.vicinityLength):
-                            if not(rule['left'][i] in (vicinity[i]+['e'])):
-                                allRulesAplicable = 0
-                                break
-                    #evolving
-                    elif (rule['operator'] == '>'):
-                        if (rule['left'] in innerObjects):
-                            innerObjects.remove(rule['left'])
-                            #innerObjects += rule['right'] #can I develop and exchange the developped object in one program?
-                        else:
+        def isProgramApplicable(self, program):
+            allRulesAplicable = 1
+            innerObjects = self.contents[:]
+            envContent = self.getEnvironmentContent()[:]
+            for rule in program:
+                # get type of a rule
+                # motion
+                if rule['operator'] in ['u', 'd', 'r', 'l']:
+                    vicinity = self.getVicinity()
+                    for i in range(self.vicinityLength):
+                        if not (rule['left'][i] in (vicinity[i] + ['e'])):
                             allRulesAplicable = 0
-                    #exchange
-                    elif (rule['operator'] =='<>'):
-                        if (rule['left'] in innerObjects) and (rule['right'] in envContent + ['e']):
-                            innerObjects.remove(rule['left'])
-                            if rule['right'] != 'e':
-                                envContent.remove(rule['right'])
-                            #innerObjects += rule['right'] # can I exchange and develop exchanged object in one program?
-                        else:
-                            allRulesAplicable = 0
-                if allRulesAplicable:
-                    aplicablePorgrams.append(program)
-            return aplicablePorgrams
-
-        def applyProgram(self):
-            applycablePrograms = self.getApplicablePrograms()
-            if applycablePrograms:
-                for rule in applycablePrograms[random.randrange(len(applycablePrograms))]:
-                    # get type of a rule
-                    # motion
-                    if rule['operator'] == 'u':
-                        self.coordinates['i'] -= 1
-                    elif rule['operator'] == 'd':
-                        self.coordinates['i'] += 1
-                    elif rule['operator'] == 'l':
-                        self.coordinates['j'] -= 1
-                    elif rule['operator'] == 'r':
-                        self.coordinates['j'] += 1
-                    # evolving
-                    elif rule['operator'] == '>':
-                        self.contents.remove(rule['left'])
-                        self.contents += rule['right']
-                    # exchange
-                    elif rule['operator'] == '<>':
-                        self.contents.remove(rule['left'])
-                        self.contents += rule['right']
+                            break
+                # evolving
+                elif (rule['operator'] == '>'):
+                    if (rule['left'] in innerObjects):
+                        innerObjects.remove(rule['left'])
+                        # innerObjects += rule['right'] #can I develop and exchange the developped object in one program?
+                    else:
+                        allRulesAplicable = 0
+                # exchange
+                elif (rule['operator'] == '<>'):
+                    if (rule['left'] in innerObjects) and (rule['right'] in envContent + ['e']):
+                        innerObjects.remove(rule['left'])
                         if rule['right'] != 'e':
-                            self.colony.envMatrix[self.coordinates['i']][self.coordinates['j']].remove(rule['right'])
-                        insert2env = {
-                            'position':self.coordinates,
-                            'symbol':rule['left']
-                        }
-                        self.colony.toConcat2env.append(insert2env)
+                            envContent.remove(rule['right'])
+                        # innerObjects += rule['right'] # can I exchange and develop exchanged object in one program?
+                    else:
+                        allRulesAplicable = 0
+            return allRulesAplicable
+
+        def getApplicablePrograms(self):
+            self.applicablePrograms = []
+            for program in self.programs:
+                if self.isProgramApplicable(program):
+                    self.applicablePrograms.append(program)
+
+        def applyRandomProgram(self):
+            if self.applicablePrograms:
+                program = self.applicablePrograms[random.randrange(len(self.applicablePrograms))]
+                if self.isProgramApplicable(program):
+                    for rule in program:
+                        # get a type of the rule
+                        # motion
+                        if rule['operator'] == 'u':
+                            self.coordinates['i'] -= 1
+                        elif rule['operator'] == 'd':
+                            self.coordinates['i'] += 1
+                        elif rule['operator'] == 'l':
+                            self.coordinates['j'] -= 1
+                        elif rule['operator'] == 'r':
+                            self.coordinates['j'] += 1
+                        # evolving
+                        elif rule['operator'] == '>':
+                            self.contents.remove(rule['left'])
+                            self.contents += rule['right']
+                        # exchange
+                        elif rule['operator'] == '<>':
+                            self.contents.remove(rule['left'])
+                            self.contents += rule['right']
+                            if rule['right'] != 'e':
+                                self.colony.envMatrix[self.coordinates['i']][self.coordinates['j']].remove(rule['right'])
+                            insert2env = {
+                                'position':self.coordinates,
+                                'symbol':rule['left']
+                            }
+                            self.colony.toConcat2env.append(insert2env)
 
