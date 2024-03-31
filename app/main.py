0e272395a9a06cb47ef433048063d32f70eac59f
Daniel <none>
2024-03-31 20:08:52 +0200

    commit

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 import extractExcel
 import colony as col
-import visualize
+import visualize2
 import sys
 import os
 
+"""
+Select the visualizer:
+0 = Without visualizer (console output only)
+1 = Tkinter-based visualizer (more detailed)
+2 = PyGame-based visualizer (faster)
+"""
+visualizer=2
 
 def main(args=sys.argv[1:]):
     if len(args):
@@ -18,20 +25,29 @@ def main(args=sys.argv[1:]):
     step = 0
     steps = colonieDefinition['parameters']['steps']
     agentCoords = [o.coordinates for o in colony.agent]
-    initplot = visualize.initVizualiser(steps, colony.rowsEnv, colony.colsEnv, agentCoords)
+    iniplot = ''
+    ## Comment line below for "visualize" visualizer 
+    if (visualizer==2): #PyGame visualizer
+        import visualize2
+        visualize = visualize2.Visualizer(colony.colsEnv * 20, colony.rowsEnv * 20, 20)  # Directly use grid size here
+        initplot = visualize.initVizualiser(steps, colony.rowsEnv, colony.colsEnv, agentCoords)
+    elif (visualizer==1):
+        import visualize #Tkinter visualizer
+        initplot = visualize.initVizualiser(steps, colony.rowsEnv, colony.colsEnv, agentCoords)
     
     while step != steps:
         step += 1
         print(step)
         colony.colonyStep()
         agentCoords = [o.coordinates for o in colony.agent]
-        popData = visualize.initPopulation(colony.rowsEnv, colony.colsEnv, agentCoords)
-        visualize.updatePlot(step, initplot, popData, colony.envMatrix,
+        if visualizer>0:
+            popData = visualize.initPopulation(colony.rowsEnv, colony.colsEnv, agentCoords)
+            visualize.updatePlot(step, initplot, popData, colony.envMatrix,
                              colonieDefinition['parameters']['animationDelay'],
                              colonieDefinition['parameters']['colorSettings']
                              )
-
-    visualize.endOfVisualization(initplot, popData)
+    if visualizer>0:
+        visualize.endOfVisualization(initplot, popData)
 
     print(colony.envMatrix) 
     for a in colony.agent: #Print the last configuration of the agent / can be commented
